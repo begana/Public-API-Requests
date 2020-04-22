061367649a2f0ad8e0b0297e7aaaf3ba60a8a479
Begana Choi <begana001.jpg@gmail.com>
2020-04-22 10:56:09 +0200

    .

--- public_api_request-v1/js/script.js
+++ public_api_request-v1/js/script.js
@@ -31,23 +31,28 @@ Promise.all([ fetchData('https://randomuser.me/api/?results=12') ])
 
 const fetchCards = (data) => {
 
-    const cards = data.map( card => `
+    const employees = data.map( employee => `
     <div class="card">
     <div class="card-img-container">
-        <img class="card-img" src="${card.picture.large}" alt="profile picture">
+        <img class="card-img" src="${employee.picture.large}" alt="profile picture">
     </div>
     <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${card.name.first} ${card.name.last}</h3>
-        <p class="card-text">${card.email}</p>
-        <p class="card-text cap">${card.location.city}, ${card.location.state}</p>
+        <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+        <p class="card-text">${employee.email}</p>
+        <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
     </div>
     </div>
     `).join('')
 
-    gellary.innerHTML = cards;
+    gellary.innerHTML = employees;
+    const cards = gellary.querySelectorAll('.card');
+    fetchModals(cards)
    
 }
 
+const fetchModals = (data) => {
+    console.log(data);
+}
 
 //----------------------------------------------------
 // EVENT HANDLERS
